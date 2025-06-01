70ba4e9e1a31df14b6dc1921fe17db9e121a26b4
victordias94 <victordias94@hotmail.com>
2025-06-01 18:27:12 -0300

    Univariate analysis

--- src/monitor_model.py
+++ src/monitor_model.py
@@ -25,6 +25,29 @@ def multivariate_analysis(cols, date_col, reference_df, analysis_df):
 
     return mv_results
 
+def univariate_analysis(cols, date_col, reference_df, analysis_df):
+
+    features_column_names = cols.split(",")
+    features_column_names = [col for col in features_column_names if col not in [date_col]]
+    
+    uv_calculator = nml.UnivariateDriftCalculator(
+        continuous_methods = ['wasserstein', 'hellinger'],
+        categorical_methods = ['jensen_shannon', 'l_infinity', 'chi2'],
+        column_names = features_column_names,
+        timestamp_column_name = date_col,
+        chunk_period = 'm'
+    )
+
+    uv_calculator.fit(reference_df)
+    uv_results = uv_calculator.calculate(analysis_df)
+
+    uv_results.filter(period = 'analysis').to_df().to_csv('output/results_univariate.csv', index = False)
+    
+    uv_figure = uv_results.filter(period = 'analysis').plot()
+    uv_figure.write_image('output/univariate.png')
+
+    return uv_results
+
 def go(args):
 
     reference = pd.read_csv(os.path.join("data", args.reference_csv))
@@ -33,6 +56,7 @@ def go(args):
     analysis[args.date_col] = pd.to_datetime(analysis[args.date_col], format='%Y%m%d').astype(str)
 
     mv_results = multivariate_analysis(args.cols, args.date_col, reference, analysis)
+    uv_results = univariate_analysis(args.cols, args.date_col, reference, analysis)
 
 
 if __name__ == '__main__':
